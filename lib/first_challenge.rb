d2527fa6dacbbbc53b9077365385f70b213caa12
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-02 22:44:38 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,5 @@
+
+# require 'pry'
 def first_challenge
   contacts = {
     "Jon Snow" => {
@@ -14,9 +16,21 @@ def first_challenge
   }
 
   #your code here
-
-
+  contacts.each do |person, data|
+    if person == "Freddy Mercury"
+      data.each do |attribute, value| 
+        if attribute == :favorite_icecream_flavors
+          value.delete("strawberry")
+        end 
+        
+      #binding.pry
+      end 
+    end
+  end 
   #remember to return your newly altered contacts hash!
   contacts
+  
 end
+
+
 
